7c868a59ec0590ff9d4de247f3d2bc7b34339023
John Choi <johnchoi1003@icloud.com>
2024-04-19 01:20:08 -0400

    Added Vite skeleton

---
+++ wikiapi-web/vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000,
+    strictPort: true,
+    // TODO: Add proxy settings later: https://vitejs.dev/config/server-options#server-proxy
+  }
+})
