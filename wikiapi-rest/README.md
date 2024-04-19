b9949a74fd25f70f65f2b9d4c3e0a326b279a2f2
John Choi <johnchoi1003@icloud.com>
2024-04-19 01:02:39 -0400

    Initial commit

---
+++ wikiapi-rest/README.md
@@ -0,0 +1,11 @@
+# WikiAPI-rest
+
+## Inbound Port
+
+* 8088
+
+## How to run this application locally
+
+* Set JDK to version 21
+* Set the environment variable to `spring.profiles.active=local` for local development.
+    - i.e. `java -jar target/wikiapi-0.0.1-SNAPSHOT.jar --spring.profiles.active=local`
---
+++ wikiapi-rest/src/main/java/io/github/osucoding/wikiapi/WikiApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.osucoding.wikiapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WikiApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WikiApiApplication.class, args);
+	}
+
+}
